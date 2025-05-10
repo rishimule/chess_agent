73debfa854a6843c1ff7c98c1600f056f0091d16
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2025-05-10 16:22:46 -0400

    Add __init__.py to chess pieces module, exposing core classes and piece types

---
+++ src/chess/pieces/__init__.py
@@ -0,0 +1,19 @@
+from .base import Color, PieceType, Piece
+from .pawn import Pawn
+from .knight import Knight
+from .bishop import Bishop
+from .rook import Rook
+from .queen import Queen
+from .king import King
+
+__all__ = [
+    'Color',
+    'PieceType',
+    'Piece',
+    'Pawn',
+    'Knight',
+    'Bishop',
+    'Rook',
+    'Queen',
+    'King'
+] 
